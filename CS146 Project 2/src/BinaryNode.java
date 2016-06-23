b60287c8d9188dfe31c87e4e6220ed423542abd1
Nicolas <nmauthes@netscape.net>
2016-06-23 15:09:21 -0700

    Project 2 upload

---
+++ CS146 Project 2/src/BinaryNode.java
@@ -0,0 +1,28 @@
+
+public class BinaryNode<E> {
+		
+	public int data;
+	public BinaryNode leftChild, rightChild;
+	
+	public BinaryNode(E data, BinaryNode lft, BinaryNode rt) {
+		data = d;
+		leftChild = lft;
+		rightChild = rt;
+	}
+	
+//	public BinaryNode() {
+//		this(null, null, null);
+//	}
+	
+	public BinaryNode<E> getLeft() {
+	
+	}
+	
+	public BinaryNode<E> getRight() {
+		
+	}
+	
+	public BinaryNode<E> getData() {
+	
+	}
+}
---
+++ CS146 Project 2/src/BinarySearchTree.java
@@ -0,0 +1,26 @@
+
+public class BinarySearchTree<E> {
+	
+	private BinaryNode root;
+	private int treeHeight;
+	
+	public void insert(E e) {
+		
+	}
+	
+	public BinaryNode find(E e) {
+	
+	}
+	
+	public boolean remove(E e) {
+	
+	}
+	
+	public int getHeight() {
+		return treeHeight;
+	}
+	
+	public E getRoot() {
+	
+	}
+}
